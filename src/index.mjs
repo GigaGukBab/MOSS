134fd2b2ed3220a2b829c1cad5e5bbcce3e8f910
Jinwoo Han <qew8502@gmail.com>
2024-07-13 22:35:11 +0900

    done for cookies, signedcookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes/index.mjs';
 import { printFigletAsync } from './figletPrint.mjs';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 // JSON 본문을 파싱하는 미들웨어
 app.use(express.json());
+app.use(cookieParser('helloWorld'));
 app.use(routes);
 
 const port = process.env.PORT || 3100;
@@ -22,6 +24,6 @@ app.listen(port, async () => {
 });
 
 app.get('/', (request, response) => {
-  response.cookie('hello', 'world', { maxAge: 99999999 });
+  response.cookie('hello', 'world', { maxAge: 30000, signed: true });
   response.status(201).send({ main_msg: "Hello to GigaGukBab's server" });
 });
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -2,9 +2,16 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/api/product', (request, response) => {
+router.get('/api/products', (request, response) => {
   console.log(request.headers.cookie);
-  response.send([{ id: 123, name: 'chicken breast', price: 12.99 }]);
+  console.log(request.cookies);
+  console.log(request.signedCookies.hello);
+  if (request.signedCookies.hello && request.signedCookies.hello === 'world')
+    return response.send([{ id: 123, name: 'chicken breast', price: 12.99 }]);
+
+  return response
+    .status(403)
+    .send({ message: 'Sorry, you need correct cookie.' });
 });
 
 export default router;
