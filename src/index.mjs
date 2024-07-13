5900a3ed669ec6940387a33472a3340b7b32044e
Jinwoo Han <qew8502@gmail.com>
2024-07-13 19:41:09 +0900

    organized routes

--- src/index.mjs
+++ src/index.mjs
@@ -12,23 +12,13 @@ figlet('Hello GigaGukBab!!', function (err, data) {
 // =================================================================================================
 
 import express from 'express';
-import usersRouter from './routes/users.mjs';
-import { mockUsers } from './utils/const.mjs';
-import { resolveIndexByUesrById } from './utils/middlewares.mjs';
+import routes from './routes/index.js';
 
 const app = express();
 
 // JSON 본문을 파싱하는 미들웨어
 app.use(express.json());
-app.use(usersRouter);
-
-const loggingMiddleware = (request, response, next) => {
-  console.log(`${request.method} - ${request.url}`);
-  next();
-};
-
-// global middleware registration
-// app.use(loggingMiddleware);
+app.use(routes);
 
 const port = process.env.PORT || 3100;
 
@@ -38,8 +28,4 @@ app.listen(port, () => {
 
 app.get('/', (request, response) => {
   response.status(201).send({ main_msg: "Hello to GigaGukBab's server" });
-});
-
-app.get('/api/product', (request, response) => {
-  response.send([{ id: 123, name: 'chicken breast', price: 12.99 }]);
 });
--- src/routes/users.mjs
+++ src/routes/users.mjs
@@ -7,9 +7,9 @@ import { createUserValidationSchema } from '../utils/validationSchema.mjs';
 import { matchedData } from 'express-validator';
 import { resolveIndexByUesrById } from '../utils/middlewares.mjs';
 
-const usersRouter = Router();
+const router = Router();
 
-usersRouter.get(
+router.get(
   '/api/users',
   checkSchema(getUserValidationSchema),
   (request, response) => {
@@ -28,7 +28,7 @@ usersRouter.get(
   }
 );
 
-usersRouter.post(
+router.post(
   '/api/users',
   checkSchema(createUserValidationSchema),
   (request, response) => {
@@ -45,45 +45,29 @@ usersRouter.post(
   }
 );
 
-usersRouter.get(
-  '/api/users/:id',
-  resolveIndexByUesrById,
-  (request, response) => {
-    const { findUserIndex } = request;
-    const findUser = mockUsers[findUserIndex];
-    if (!findUser) return response.sendStatus(404);
-    return response.send(findUser);
-  }
-);
+router.get('/api/users/:id', resolveIndexByUesrById, (request, response) => {
+  const { findUserIndex } = request;
+  const findUser = mockUsers[findUserIndex];
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
+});
 
-usersRouter.put(
-  '/api/users/:id',
-  resolveIndexByUesrById,
-  (request, response) => {
-    const { body, findUserIndex } = request;
-    mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
-    return response.sendStatus(200);
-  }
-);
+router.put('/api/users/:id', resolveIndexByUesrById, (request, response) => {
+  const { body, findUserIndex } = request;
+  mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
+  return response.sendStatus(200);
+});
 
-usersRouter.patch(
-  '/api/users/:id',
-  resolveIndexByUesrById,
-  (request, response) => {
-    const { body, findUserIndex } = request;
-    mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
-    return response.sendStatus(200);
-  }
-);
+router.patch('/api/users/:id', resolveIndexByUesrById, (request, response) => {
+  const { body, findUserIndex } = request;
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+  return response.sendStatus(200);
+});
 
-usersRouter.delete(
-  '/api/users/:id',
-  resolveIndexByUesrById,
-  (request, response) => {
-    const { findUserIndex } = request;
-    mockUsers.splice(findUserIndex, 1);
-    return response.sendStatus(200);
-  }
-);
+router.delete('/api/users/:id', resolveIndexByUesrById, (request, response) => {
+  const { findUserIndex } = request;
+  mockUsers.splice(findUserIndex, 1);
+  return response.sendStatus(200);
+});
 
-export default usersRouter;
+export default router;
